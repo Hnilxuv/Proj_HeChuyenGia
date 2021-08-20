cda2806e7a092c047adfa67e55e1a87a71eadedc
Đức Linh <soigia0601@gmail.com>
2021-08-20 19:10:25 +0700

    load phan ung

--- DieuCheHoaHoc/DataTriThuc.cs
+++ DieuCheHoaHoc/DataTriThuc.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,9 +25,50 @@ namespace DieuCheHoaHoc
             // lay data tu file
             phanUngs = new List<PhanUng>();
 
+            ArrayList list = new ArrayList();
+
             StreamReader sr = new StreamReader(triThucPath);
             while (!sr.EndOfStream) {
-                phanUngs.Add(new PhanUng(sr.ReadLine()));
+                list.Add(sr.ReadLine());
+                //phanUngs.Add(new PhanUng(sr.ReadLine()));
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                int j = i + 1;
+                while (j < list.Count)
+                {
+                    var vti = list[i].ToString().Split('-');
+                    var vtj = list[j].ToString().Split('-');
+                    var vtit = vti[1].Split(' ');
+                    var vtjt = vtj[1].Split(' ');
+
+                    int m = 1, n = 1;
+                    while (vtit[0] == "(" && vtit[m] != ")" && m < vtit.Count() - 1)
+                    {
+                        vtit[0] += vtit[m];
+                        m++;
+                    }
+                    while (vtjt[0] == "(" && vtjt[n] != ")" && n < vtjt.Count() - 1)
+                    {
+                        vtjt[0] += vtjt[n];
+                        n++;
+                    }
+                    if (vti[0] + vtit[0] == vtj[0] + vtjt[0])
+                    {
+                        list[i] = list[i] + " " + vtj.Last().Split(' ').Last();
+                        list.RemoveAt(j);
+                    }
+                    else
+                    {
+                        j++;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                phanUngs.Add(new PhanUng(list[i].ToString()));
+                //Console.WriteLine(list[i].ToString());
             }
             sr.Close();
         }
